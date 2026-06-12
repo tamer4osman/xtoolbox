f109c45ae40b2339d9073f0dabc50352a7386a68
Tamer Osman <tamer4osman@gmail.com>
2026-06-12 11:57:18 +0200

    test(text-diff): import diffLines from source instead of duplicating

--- src/__tests__/text-diff.test.js
+++ src/__tests__/text-diff.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { toolConfig, initTextDiff } from '../tools/text/text-diff.js';
+import { toolConfig, initTextDiff, diffLines } from '../tools/text/text-diff.js';
 
 describe('text-diff', () => {
   describe('toolConfig', () => {
@@ -17,34 +17,7 @@ describe('text-diff', () => {
   });
 });
 
-describe('text-diff: internal functions', () => {
-  function diffLines(oldText, newText) {
-    const oldLines = oldText.split('\n');
-    const newLines = newText.split('\n');
-    const result = [];
-
-    let i = 0, j = 0;
-    while (i < oldLines.length || j < newLines.length) {
-      if (i >= oldLines.length) {
-        result.push({ type: 'added', line: newLines[j++] });
-      } else if (j >= newLines.length) {
-        result.push({ type: 'removed', line: oldLines[i++] });
-      } else if (oldLines[i] === newLines[j]) {
-        result.push({ type: 'unchanged', line: oldLines[i++] });
-        j++;
-      } else {
-        const foundInNew = newLines.indexOf(oldLines[i], j);
-        const foundInOld = oldLines.indexOf(newLines[j], i);
-        if (foundInNew !== -1 && (foundInOld === -1 || foundInNew - j <= foundInOld - i)) {
-          result.push({ type: 'added', line: newLines[j++] });
-        } else {
-          result.push({ type: 'removed', line: oldLines[i++] });
-        }
-      }
-    }
-    return result;
-  }
-
+describe('text-diff: diffLines', () => {
   function escapeHtml(str) {
     return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
   }
